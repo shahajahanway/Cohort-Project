6db1989d5ae435fe40c3f1fc6dcb2c675799c88b
Shahajahan Sheikh <167621740+shahajahanway@users.noreply.github.com>
2025-02-25 22:05:35 +0530

    Add files via upload

---
+++ Day 21/script.js
@@ -0,0 +1,13 @@
+// console.log("hello world");
+// console.warn("hello world")
+// console.info("hello world")
+// console.error("hello world")
+// console.table("hello world");
+
+// console.log(35*2-(10/2)+7);
+console.log(typeof"123");
+
+console.log(typeof123);
+console.log(typeof true);
+
+console.log(typeof null);
