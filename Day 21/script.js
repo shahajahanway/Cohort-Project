e9b854b0e98ce8a34603339c51968db150571b8d
Shahajahan Sheikh <167621740+shahajahanway@users.noreply.github.com>
2025-02-26 23:15:44 +0530

    Add files via upload

--- Day 21/script.js
+++ Day 21/script.js
@@ -1,13 +1,85 @@
-// console.log("hello world");
-// console.warn("hello world")
-// console.info("hello world")
-// console.error("hello world")
-// console.table("hello world");
+// 1. Log "Hello, JavaScript!" to the console in 3 different ways.
+// console.log("Hello World");
+// console.error("Hello World");
+// console.info("Hello World");
+// console.table({name: "Hello shaha", age: 22, email: "contact@shahajahan.in"});
 
-// console.log(35*2-(10/2)+7);
-console.log(typeof"123");
 
-console.log(typeof123);
-console.log(typeof true);
 
-console.log(typeof null);
+
+// 2. Perform 35 * 2 - (10 / 2) + 7 and log the result.
+// let result = 35 * 2 - (10 / 2) + 7 ;
+// console.log(result);
+
+
+
+
+// 3. Log the data type of "123", 123, true, and null using typeof.
+// console.log(typeof("123"));
+// console.log(typeof(123));
+// console.log(typeof(true));
+// console.log(typeof(null));
+
+
+
+
+// 4. Write a program that swaps the values of two variables.
+// let a = 12;
+// let b = 13;
+
+// let c = a;
+// a = b;
+// b = c;
+
+// let a = 10 , b = 20;
+// console.log("Before Swap:", "a =", a, ", b =", b);
+
+// let temp = a;
+// a = b;
+// b = temp;
+// console.log("After Swap (Method 1):", "a =", a, ", b =",b);
+
+
+
+
+// 5. Use console.group() to organize logs into a group.
+// console.group("User Info");
+// console.log("Name: shaha");
+// console.log("Age: 22");
+// console.log("City: Siliguri");
+// console.groupEnd();
+
+// console.groupCollapsed("Callapsed Group Example")
+// console.log("This content is collapsed by default.");
+// console.groupEnd();
+
+
+
+
+// 6. Declare a const object, modify its properties, and log the updated object.
+// const person = {name: "Shahajahan", age: 20, city: "Siliguri" }
+// console.log("Before Update",person);
+
+// person.age = 22;
+// person.city = "Shree Balaji";
+
+// person.country = "India";
+// console.log("After Update " ,person);
+
+
+
+
+// 7. Convert "50" (string) into a number using 3 different methods.
+// let str = "50";
+
+// let num1 = Number(str);
+// console.log(num1, typeof num1);
+
+
+
+
+// 8. Check if "JavaScript" contains "Script" without using .includes().
+// let str =  "JavaScript";
+
+// console.log(str.indexOf("Script")!== -1);
+
